47c9cae0e6972c2b197c47c130cbe267ec0da8a2
TobiasTac <tobias.vida@live.com>
2025-03-07 14:15:17 -0300

    first commit

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import { ButtonContainer, Form } from './styles';
+
+import FormGroup from '../FormGroup';
+
+import Button from '../Button';
+import Input from '../Input';
+import Select from '../Select';
+
+export default function ContactForm({ buttonLabel }) {
+  return (
+    <Form>
+      <FormGroup>
+        <Input placeholder="Nome" />
+      </FormGroup>
+
+      <FormGroup
+        error="O formato do e-mail é inválido."
+      >
+        <Input placeholder="E-mail" error />
+      </FormGroup>
+
+      <FormGroup>
+        <Input placeholder="Telefone" />
+      </FormGroup>
+
+      <FormGroup>
+        <Select>
+          <option value="instagram"> Instagram </option>
+        </Select>
+      </FormGroup>
+
+      <ButtonContainer>
+        <Button type="submit">
+          {buttonLabel}
+        </Button>
+      </ButtonContainer>
+
+    </Form>
+  );
+}
+
+ContactForm.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
