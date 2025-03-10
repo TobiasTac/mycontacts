b14a997c14c9bfb766827bcf4380fceb7b1a18e5
TobiasTac <tobias.vida@live.com>
2025-03-10 19:51:56 -0300

    feat: add mask in phone input

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 import useErrors from '../../hooks/useErrors';
+import { formatPhone } from '../../utils/formatPhone';
 import { isEmailValid } from '../../utils/isEmailValid';
 
 import { ButtonContainer, Form } from './styles';
@@ -40,11 +41,15 @@ export default function ContactForm({ buttonLabel }) {
     }
   }
 
+  function handlePhoneChange(event) {
+    setPhone(formatPhone(event.target.value));
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
 
     console.log({
-      name, email, phone, category,
+      name, email, phone: phone.replace(/\D/g, ''), category,
     });
   }
 
@@ -73,7 +78,8 @@ export default function ContactForm({ buttonLabel }) {
         <Input
           placeholder="Telefone"
           value={phone}
-          onChange={(event) => setPhone(event.target.value)}
+          onChange={handlePhoneChange}
+          maxLength="15"
         />
       </FormGroup>
 
