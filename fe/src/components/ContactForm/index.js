ae3320a410dcb948c474455e30dc9b8c4f5e25cd
TobiasTac <tobias.vida@live.com>
2025-04-01 18:30:41 -0300

    Corrigindo fe - removendo submódulo e adicionando como pasta normal

---
+++ fe/src/components/ContactForm/index.js
@@ -0,0 +1,138 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+
+import useErrors from '../../hooks/useErrors';
+import CategoriesService from '../../services/CategoriesService';
+import { formatPhone } from '../../utils/formatPhone';
+import { isEmailValid } from '../../utils/isEmailValid';
+
+import { ButtonContainer, Form } from './styles';
+
+import FormGroup from '../FormGroup';
+
+import Button from '../Button';
+import Input from '../Input';
+import Select from '../Select';
+
+export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [categoryId, setCategoryId] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+
+  const {
+    errors,
+    setError,
+    removeError,
+    getErrorMessageByFieldName,
+  } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
+
+  useEffect(() => {
+    async function loadCategories() {
+      try {
+        const categoriesList = await CategoriesService.listCategories();
+
+        setCategories(categoriesList);
+      } catch { } finally {
+        setIsLoadingCategories(false);
+      }
+    }
+
+    loadCategories();
+  }, []);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório.' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'email', message: 'Email inválido' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handlePhoneChange(event) {
+    setPhone(formatPhone(event.target.value));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone: phone.replace(/\D/g, ''), categoryId,
+    });
+  }
+
+  return (
+    <Form onSubmit={handleSubmit} noValidate>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          error={getErrorMessageByFieldName('name')}
+          placeholder="Nome *"
+          value={name}
+          onChange={handleNameChange}
+        />
+      </FormGroup>
+
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          type="email"
+          error={getErrorMessageByFieldName('email')}
+          placeholder="E-mail"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={handlePhoneChange}
+          maxLength="15"
+        />
+      </FormGroup>
+
+      <FormGroup isLoading={isLoadingCategories}>
+        <Select
+          value={categoryId}
+          onChange={(event) => setCategoryId(event.target.value)}
+          disabled={isLoadingCategories}
+        >
+          <option value=""> Sem Categoria </option>
+
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </Select>
+      </FormGroup>
+
+      <ButtonContainer>
+        <Button type="submit" disabled={!isFormValid}>
+          {buttonLabel}
+        </Button>
+      </ButtonContainer>
+
+    </Form>
+  );
+}
+
+ContactForm.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
---
+++ fe/src/components/Loader/index.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+import { Overlay } from './styles';
+
+export default function Loader({ isLoading }) {
+  if (!isLoading) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
+    <Overlay>
+      <div className="loader" />
+    </Overlay>,
+    document.getElementById('loader-root'),
+  );
+}
+
+Loader.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+};
