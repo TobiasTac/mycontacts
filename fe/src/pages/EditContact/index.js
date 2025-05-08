098c1079e105acecc09a661e7a793a1d7ee76a30
TobiasTac <tobias.vida@live.com>
2025-05-08 20:52:43 -0300

    feat: uploading contact data to the EditContact page

--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -1,15 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
 import PageHeader from '../../components/PageHeader';
 
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+
 export default function EditContact() {
+  const [isLoading, setIsloading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(
+          id,
+        );
+
+        console.log({ contactData });
+        setIsloading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader
         title="Editar Tobias Alves"
       />
 
       <ContactForm
         buttonLabel="Salvar Alterações"
+        onSubmit={handleSubmit}
       />
     </>
   );
