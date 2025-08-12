797fccbe225adeaab1cc2b558ead045a70858661
TobiasTac <tobias.vida@live.com>
2025-08-12 12:02:41 -0300

    refactor: componentizing Home

---
+++ fe/src/pages/Home/components/SearchNotFound/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+
+import PropTypes from 'prop-types';
+
+import magnifierQuestion from '../../../../assets/images/magnifier-question.svg';
+
+import { Container } from './styles';
+
+export default function SearchNotFound({ searchTerm }) {
+  return (
+    <Container>
+      <img src={magnifierQuestion} alt="Magnifier question" />
+
+      <span>
+        Nenhum contato foi encontrado para <strong>{searchTerm}</strong>.
+      </span>
+    </Container>
+  );
+}
+
+SearchNotFound.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+};
--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,28 +1,13 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-/* eslint-disable no-nested-ternary */
+import { Container } from './styles';
 
-import { Link } from 'react-router-dom';
-import {
-  Card,
-  Container,
-  EmptyListContainer,
-  ErrorContainer,
-  Header,
-  InputSearchContainer,
-  ListHeader,
-  SearchNotFoundCointainer,
-} from './styles';
-
-import emptyBox from '../../assets/images/empty-box.svg';
-import arrow from '../../assets/images/icons/arrow.svg';
-import edit from '../../assets/images/icons/edit.svg';
-import trash from '../../assets/images/icons/trash.svg';
-import magnifierQuestion from '../../assets/images/magnifier-question.svg';
-import sad from '../../assets/images/sad.svg';
-
-import Button from '../../components/Button';
 import Loader from '../../components/Loader';
 import Modal from '../../components/Modal';
+import ContactsList from './components/ContactsList';
+import EmptyList from './components/EmptyList';
+import ErrorStatus from './components/ErrorStatus';
+import Header from './components/Header';
+import InputSearch from './components/InputSearch';
+import SearchNotFound from './components/SearchNotFound';
 import useHome from './useHome';
 
 export default function Home() {
@@ -48,127 +33,49 @@ export default function Home() {
     <Container>
       <Loader isLoading={isLoading} />
 
-      <Modal
-        danger
-        isLoading={isLoadingDelete}
-        visible={isDeleteModalVisible}
-        title={`Tem certeza que deseja remover o contato "${contactBeingDeleted?.name}"?`}
-        confirmLabel="Deletar"
-        onCancel={handleCloseDeleteModal}
-        onConfirm={handleConfirmDeleteContact}
-      >
-        <p> Esta ação não poderá ser desfeita! </p>
-      </Modal>
-
       {contacts.length > 0 && (
-        <InputSearchContainer>
-          <input
-            value={searchTerm}
-            type="text"
-            placeholder="Pesquisar Contato"
-            onChange={handleChangeSearchTerm}
-          />
-        </InputSearchContainer>
+        <InputSearch
+          value={searchTerm}
+          onChange={handleChangeSearchTerm}
+        />
       )}
 
       <Header
-        $justifyContent={
-          hasError
-            ? 'flex-end'
-            : (
-              contacts.length > 0
-                ? 'space-between'
-                : 'center'
-            )
-        }
-      >
-        {(!hasError && contacts.length > 0) && (
-          <strong>
-            {filteredContacts.length}
-            {filteredContacts.length === 1 ? ' contato' : ' contatos'}
-          </strong>
-        )}
-        <Link to="/new"> Novo Contato </Link>
-      </Header>
+        hasError={hasError}
+        qtyOfContacts={contacts.length}
+        qtyOfFilteredContacts={filteredContacts.length}
+      />
 
-      {
-        hasError && (
-          <ErrorContainer>
-            <img src={sad} alt="Sad" />
-
-            <div className="details">
-              <strong>Ocorreu um erro ao obter os seus contatos!</strong>
-
-              <Button type="button" onClick={handleTryAgain}>
-                Tentar novamente
-              </Button>
-            </div>
-          </ErrorContainer>
-        )
-      }
+      {hasError && <ErrorStatus onTryAgain={handleTryAgain} />}
 
       {!hasError && (
         <>
           {(contacts.length < 1 && !isLoading) && (
-            <EmptyListContainer>
-              <img src={emptyBox} alt="Empty box" />
-              <p>
-                Você ainda não tem nenhum contato cadastrado!
-                Clique no botão <strong> ”Novo contato” </strong> à cima
-                para cadastrar o seu primeiro!
-              </p>
-            </EmptyListContainer>
+            <EmptyList />
           )}
 
           {(contacts.length > 0 && filteredContacts.length < 1) && (
-            <SearchNotFoundCointainer>
-              <img src={magnifierQuestion} alt="Magnifier question" />
-
-              <span>
-                Nenhum contato foi encontrado para <strong>{searchTerm}</strong>.
-              </span>
-            </SearchNotFoundCointainer>
+            <SearchNotFound searchTerm={searchTerm} />
           )}
 
-          {
-            filteredContacts.length > 0 && (
-              <ListHeader $orderBy={orderBy}>
-                <button type="button" onClick={handleToggleOrderBy}>
-                  <span> Nome </span>
-                  <img src={arrow} alt="Arrow" />
-                </button>
-              </ListHeader>
-            )
-          }
+          <ContactsList
+            filteredContacts={filteredContacts}
+            orderBy={orderBy}
+            onToggleOrderBy={handleToggleOrderBy}
+            onDeleteContact={handleDeleteContact}
+          />
 
-          {
-            filteredContacts.map((contact) => (
-              <Card key={contact.id}>
-                <div className="info">
-                  <div className="contact-name">
-                    <strong>{contact.name}</strong>
-                    {contact.category.name && (
-                      <small>{contact.category.name}</small>
-                    )}
-                  </div>
-                  <span>{contact.email}</span>
-                  <span>{contact.phone}</span>
-                </div>
-
-                <div className="actions">
-                  <Link to={`/edit/${contact.id}`}>
-                    <img src={edit} alt="Edit" />
-                  </Link>
-                  <button
-                    type="button"
-                    onClick={() => handleDeleteContact(contact)}
-                  >
-                    <img src={trash} alt="Delete" />
-                  </button>
-                </div>
-              </Card>
-            ))
-          }
+          <Modal
+            danger
+            isLoading={isLoadingDelete}
+            visible={isDeleteModalVisible}
+            title={`Tem certeza que deseja remover o contato "${contactBeingDeleted?.name}"?`}
+            confirmLabel="Deletar"
+            onCancel={handleCloseDeleteModal}
+            onConfirm={handleConfirmDeleteContact}
+          >
+            <p> Esta ação não poderá ser desfeita! </p>
+          </Modal>
         </>
       )}
     </Container>
