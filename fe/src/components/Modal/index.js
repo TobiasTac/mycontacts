75ae9141a6366f7dd59acc7c6ee407ee07e055e7
TobiasTac <tobias.vida@live.com>
2025-08-12 19:14:52 -0300

    feat: animation modal

--- fe/src/components/Modal/index.js
+++ fe/src/components/Modal/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import { useEffect, useRef, useState } from 'react';
 import { Container, Footer, Overlay } from './styles';
 
 import Button from '../Button';
@@ -16,14 +17,39 @@ export default function Modal({
   onCancel,
   onConfirm,
 }) {
-  if (!visible) {
+  const [shouldRender, setShouldRender] = useState(visible);
+
+  const overlayRef = useRef(null);
+
+  useEffect(() => {
+    if (visible) {
+      setShouldRender(true);
+    }
+
+    function handleAnimationEnd() {
+      setShouldRender(false);
+    }
+
+    const overlatRefElement = overlayRef.current;
+    if (!visible && overlayRef.current) {
+      overlatRefElement.addEventListener('animationend', handleAnimationEnd);
+    }
+
+    return () => {
+      if (overlatRefElement) {
+        overlatRefElement.removeEventListener('animationend', handleAnimationEnd);
+      }
+    };
+  }, [visible]);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="modal-root">
-      <Overlay>
-        <Container $danger={danger}>
+      <Overlay $isLeaving={visible} ref={overlayRef}>
+        <Container $danger={danger} $isLeaving={visible}>
           <h1>{title}</h1>
 
           <div className="modal-body">
