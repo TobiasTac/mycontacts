af0739e623e9b70a2da4f5f82ad8f90f51d41804
TobiasTac <tobias.vida@live.com>
2025-08-11 21:46:35 -0300

    refactor: containerpresentation editcontact

---
+++ fe/src/pages/EditContact/useEditContact.js
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+
+export default function useEditContact() {
+  const [isLoading, setIsloading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(
+          id,
+        );
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setIsloading(false);
+          setContactName(contact.name);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado!',
+          });
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history, safeAsyncAction]);
+
+  async function handleSubmit(contact) {
+    try {
+      const updatedContactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(updatedContactData.name);
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato',
+      });
+    }
+  }
+
+  return {
+    isLoading,
+    contactName,
+    contactFormRef,
+    handleSubmit,
+  };
+}
