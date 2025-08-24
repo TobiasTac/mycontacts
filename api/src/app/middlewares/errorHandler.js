7e3c905f699a0c285a827ef2a898443ba6be99d9
TobiasTac <tobias.vida@live.com>
2025-08-24 18:16:14 -0300

    update versions

--- api/src/app/middlewares/errorHandler.js
+++ api/src/app/middlewares/errorHandler.js
@@ -1,5 +1,4 @@
 module.exports = (error, request, response, next) => {
-  console.log('#### Error Handler');
-  console.log(error);
+  console.log('errorHandler', error);
   response.sendStatus(500);
 };
